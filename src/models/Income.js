f20d68b5e0f69de0c4be85f44a3295191892c8ed
Aayush <aayushbastola772@gmail.com>
2025-12-02 21:04:14 +0545

    income model with payment mode

---
+++ src/models/Income.js
@@ -0,0 +1,94 @@
+// src/models/Income.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const User = require('./User');
+const UserIncomeCategory = require('./UserIncomeCategory');
+const PaymentMode = require("./PaymentMode");
+
+const Income = sequelize.define(
+  'Income',
+  {
+    incomeId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      field: 'income_id',
+    },
+
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      field: 'user_id',
+      references: {
+        model: User,
+        key: 'userId',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+
+    incomeAmount: {
+      type: DataTypes.DECIMAL(12, 2),
+      allowNull: false,
+      field: 'income_amount',
+    },
+
+    categoryId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      field: 'category_id',
+      references: {
+        model: UserIncomeCategory,
+        key: 'userIncomeCategoryId',
+      },
+      onDelete: 'SET NULL',
+      onUpdate: 'CASCADE',
+    },
+
+    payementModeId:{
+        type: DataTypes.UUID,
+      allowNull: false,
+      field: 'payment_mode_id',
+      references: {
+        model: PaymentMode,
+        key: 'PaymentModeId',
+      },
+      onDelete: 'SET NULL',//set PaymentModeId null when parent is deleted
+      onUpdate: 'CASCADE',
+
+    },
+
+    remarks:{
+        type: DataTypes.STRING,
+        allowNull: true,
+
+    },
+
+    path: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      comment: 'Path or URL of the uploaded receipt image',
+    },
+
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+
+  },
+
+  {
+    tableName: 'incomes',
+    timestamps: true, // createdAt, updatedAt
+  }
+  
+);
+
+// Relationships
+User.hasMany(Income, { foreignKey: 'userId' });
+Income.belongsTo(User, { foreignKey: 'userId' });
+
+UserIncomeCategory.hasMany(Income, { foreignKey: 'categoryId' });
+Income.belongsTo(UserIncomeCategory, { foreignKey: 'categoryId' });
+
+module.exports = Income;

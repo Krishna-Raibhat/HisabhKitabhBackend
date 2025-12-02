ef69d267dfe42c91c3be6b2dca8f00599da40e16
Aayush <aayushbastola772@gmail.com>
2025-12-02 21:17:02 +0545

    added remarks and paymentMode

--- src/models/Expense.js
+++ src/models/Expense.js
@@ -3,6 +3,7 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 const User = require('./User');
 const UserExpenseCategory = require('./UserExpenseCategory');
+const PaymentMode = require("./PaymentMode");
 
 const Expense = sequelize.define(
   'Expense',
@@ -13,6 +14,7 @@ const Expense = sequelize.define(
       primaryKey: true,
       field: 'expense_id',
     },
+
     userId: {
       type: DataTypes.UUID,
       allowNull: false,
@@ -24,11 +26,13 @@ const Expense = sequelize.define(
       onDelete: 'CASCADE',
       onUpdate: 'CASCADE',
     },
+    
     expenseAmount: {
       type: DataTypes.DECIMAL(12, 2),
       allowNull: false,
       field: 'expense_amount',
     },
+
     categoryId: {
       type: DataTypes.UUID,
       allowNull: false,
@@ -40,16 +44,40 @@ const Expense = sequelize.define(
       onDelete: 'SET NULL',
       onUpdate: 'CASCADE',
     },
+
+    payementModeId:{
+        type: DataTypes.UUID,
+        allowNull: false,
+        field: 'payment_mode_id',
+        references: {
+            model: PaymentMode,
+            key: 'PaymentModeId',
+        },
+        onDelete: 'SET NULL',//set PaymentModeId null when parent is deleted
+        onUpdate: 'CASCADE',
+
+    },
+
+    remarks:{
+        type: DataTypes.STRING,
+        allowNull: true,
+
+    },
+    
+
     path: {
       type: DataTypes.STRING,
       allowNull: true,
       comment: 'Path or URL of the uploaded receipt image',
     },
+
     date: {
       type: DataTypes.DATEONLY,
       allowNull: false,
     },
+
   },
+
   {
     tableName: 'expenses',
     timestamps: true, // createdAt, updatedAt
@@ -62,5 +90,9 @@ Expense.belongsTo(User, { foreignKey: 'userId' });
 
 UserExpenseCategory.hasMany(Expense, { foreignKey: 'categoryId' });
 Expense.belongsTo(UserExpenseCategory, { foreignKey: 'categoryId' });
+
+PaymentMode.hasMany(Expense,{foreignKey:'paymentModeId'});
+Expense.belongsTo(PaymentMode,{foreignKey:'paymetModeId'});
+
 
 module.exports = Expense;

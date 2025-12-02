656cc6440c130e72c00bebd05156e70b0471dfe7
Aayush <aayushbastola772@gmail.com>
2025-12-02 19:46:37 +0545

    income/expense category model

---
+++ src/models/UserExpenseCategory.js
@@ -0,0 +1,48 @@
+// src/models/UserExpenseCategory.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const User = require('./User');
+
+const UserExpenseCategory = sequelize.define(
+  'UserExpenseCategory',
+  {
+    userExpenseCategoryId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      field: 'user_expense_category_id',
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      field: 'user_id',
+      references: {
+        model: User,
+        key: 'userId',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    userCategoryName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: 'user_category_name',
+    },
+    isDefault: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+      field: 'is_default',
+    },
+  },
+  {
+    tableName: 'user_expense_categories',
+    timestamps: true, // createdAt, updatedAt
+  }
+);
+
+// Relationships
+User.hasMany(UserExpenseCategory, { foreignKey: 'userId' });
+UserExpenseCategory.belongsTo(User, { foreignKey: 'userId' });
+
+module.exports = UserExpenseCategory;

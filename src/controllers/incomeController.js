5db1f38e2c82d6049700e55199a5b2f5e5af67d4
Aayush <aayushbastola772@gmail.com>
2025-12-03 18:41:51 +0545

    get income balance/details + chart

--- src/controllers/incomeController.js
+++ src/controllers/incomeController.js
@@ -1,5 +1,6 @@
 const incomeService = require('../services/incomeService');
 
+//add income
 const createIncome = async (req, res) =>{
     try{
         const{
@@ -35,6 +36,85 @@ const createIncome = async (req, res) =>{
     
 };
 
+//get total income balance
+const totalIncome = async (req, res) => {
+    try {
+        const total = await incomeService.getTotalIncome(req.user.userId);
+        res.json({ success: true, totalIncome: total || 0 });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
+    
+//get daily income balance
+const dailyIncome = async (req, res) => {
+    try {
+        const total = await incomeService.getDailyIncome(
+            req.user.userId,
+            req.query.date
+        );
+        res.json({ success: true, dailyIncome: total || 0 });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
+
+//get monthly income balance
+const monthlyIncome = async (req, res) => {
+    try {
+        const total = await incomeService.getMonthlyIncome(
+            req.user.userId,
+            req.query.year,
+            req.query.month
+        );
+        res.json({ success: true, monthlyIncome: total || 0 });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
+
+//get all the income details of all time
+const incomeDetails = async (req, res) => {
+    try {
+        const data = await incomeService.getAllIncomeDetails(req.user.userId);
+        res.json({ success: true, data });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
+
+    
+
+//charts
+const last7DaysChart = async (req, res) => {
+    try {
+        const data = await incomeService.getLast7DaysIncomeChart(req.user.userId);
+        res.json({ success: true, chart: data });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
+
+const monthlyIncomeChart = async (req, res) => {
+    try {
+        const data = await incomeService.getMonthlyIncomeChart(
+            req.user.userId,
+            req.query.year
+        );
+        res.json({ success: true, chart: data });
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
+
+
 module.exports ={
     createIncome,
+    totalIncome,
+    dailyIncome,
+    monthlyIncome,
+    incomeDetails,
+    last7DaysChart,
+    monthlyIncomeChart
+
 }
--- src/services/incomeService.js
+++ src/services/incomeService.js
@@ -1,10 +1,88 @@
 const Income = require('../models/Income');
+const { Op, Sequelize } = require("sequelize");
 
 const createIncome = async(data) =>{
     const newIncome = await Income.create(data);
     return newIncome;
 };
 
+// Totals
+const getTotalIncome = async (userId) => {
+    return await Income.sum("incomeAmount", { where: { userId } });
+};
+
+const getDailyIncome = async (userId, date) => {
+    return await Income.sum("incomeAmount", {
+        where: { userId, date }
+    });
+};
+
+const getMonthlyIncome = async (userId, year, month) => {
+    const startDate = `${year}-${month}-01`;
+    const endDate = `${year}-${month}-31`;
+
+    return await Income.sum("incomeAmount", {
+        where: {
+        userId,
+        date: { [Op.between]: [startDate, endDate] }
+        }
+    });
+};
+
+// All income details
+const getAllIncomeDetails = async (userId) => {
+    return await Income.findAll({
+        where: { userId },
+       include: ["UserIncomeCategory", "PaymentMode"],
+        order: [["date", "DESC"]],
+    });
+};
+
+// Charts
+const getLast7DaysIncomeChart = async (userId) => {
+    const today = new Date();
+    const start = new Date();
+    start.setDate(today.getDate() - 6);
+
+    return await Income.findAll({
+        where: {
+        userId,
+        date: { [Op.between]: [start, today] }
+        },
+        attributes: [
+            "date",
+            [Sequelize.fn("SUM", Sequelize.col("income_amount")), "totalIncome"]
+            ],
+        group: ["date"],
+        order: [["date", "ASC"]],
+    });
+};
+
+const getMonthlyIncomeChart = async (userId, year) => {
+    return await Income.findAll({
+        where: {
+            userId,
+            date: {
+                [Op.between]: [`${year}-01-01`, `${year}-12-31`]
+            }
+        },
+        attributes: [
+            [Sequelize.fn("MONTH", Sequelize.col("date")), "month"],
+            [Sequelize.fn("SUM", Sequelize.col("income_amount")), "totalIncome"]
+        ],
+        group: ["month"],
+        order: [["month", "ASC"]],
+    });
+};
+
+// Export all
+
 module.exports ={
     createIncome,
+    getTotalIncome,
+    getDailyIncome,
+    getMonthlyIncome,
+    getAllIncomeDetails,
+    getLast7DaysIncomeChart,
+    getMonthlyIncomeChart
 };

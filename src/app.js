150973c59241539f1434c6c7d7033d7df4eebd19
RawalAnuma <rawalanuma@gmail.com>
2025-12-03 17:47:58 +0545

    update app.js and register income routes & models

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const sequelize = require('./config/database');
+
+//Routes
 const authRoutes = require('./routes/authRoutes');
 const balanceRoutes = require('./routes/balanceRoutes');
+const incomeRoutes = require('./routes/incomeRoutes');
 
 dotenv.config();
 const app = express();
@@ -21,6 +24,7 @@ app.use(express.json());
     // Make sure models are loaded before sync
     require('./models/User');
     require('./models/Balance');
+    require('./models/Income');
 
     await sequelize.sync(); // or { alter: true } during development
     console.log('Database synced.');
@@ -32,6 +36,7 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/balance', balanceRoutes);
+app.use('/api/income', incomeRoutes);
 
 // Test route
 app.get('/', (req, res) => {
